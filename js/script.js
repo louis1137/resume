154f09e004e7dec322f79833147ba88867b3e472
hellolouis <monkey0120@nate.com>
2024-03-09 19:29:28 +0900

    다크모드

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+	
+	document.querySelector('.toggle').addEventListener('click', e => {
+		if(document.body.classList[0] == 'dark') {
+			document.body.removeAttribute('class')
+		} else {
+			document.body.className = 'dark'
+		}
+	})
+
+});
